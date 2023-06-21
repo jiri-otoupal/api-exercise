6fb4ba117d3704b4d6f612e6859a939c70445550
jiri_otoupal <Lkw2Rn89p7yNJUMRD4fv>
2023-06-21 12:36:59 +0200

    test change

--- tests/test_base.py
+++ tests/test_base.py
@@ -11,8 +11,8 @@ class BaseCallsCase(unittest.TestCase):
 
     def test_root(self):
         response = client.get("/")
-        assert response.status_code == 200
-        assert response.json() == {"msg": "Welcome to Car Reservation System!"}
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"msg": "Welcome to Car Reservation System!"})
 
 
 if __name__ == '__main__':
